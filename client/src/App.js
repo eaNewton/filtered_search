b9d873818dbe45ea93902a3435be79ddbe7f066b
eaNewton <emmett.newton@praxent.com>
2019-04-21 12:17:00 -0500

    link front and back ends

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <NavBar />
+        <p className="AppIntro">{this.state.apiResponse}</p>
       </div>
     )
   }
