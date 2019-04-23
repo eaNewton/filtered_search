964caa51e425cfd685cc69a5ebb19d07941c2180
eaNewton <emmett.newton@praxent.com>
2019-04-23 15:53:27 -0500

    stopping point

--- api/app.js
+++ api/app.js
@@ -1,15 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testAPIRouter = require('./routes/testAPI');
 
-var app = express();
+const app = express();
+
+const customers = require('./data/customers.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
+
+app.get('/api/customers', function(req, res) {
+  res.json(customers);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/data/customers.js
@@ -0,0 +1,19 @@
+const customers = [
+  {
+    customer_id: 1,
+    first_name: 'John',
+    last_name: "Cougar Mellencamp",
+  },
+  {
+    customer_id: 2,
+    first_name: 'James',
+    last_name: 'Hendrix',
+  },
+  {
+    customer_id: 3,
+    first_name: 'Tom',
+    last_name: 'Jones',
+  },
+];
+
+module.exports = customers;
